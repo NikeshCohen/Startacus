b560456e9254d69c9379b86873b1ebe33e340af2
nikeshcohen <nikeshcohen@outlook.com>
2025-04-30 20:25:52 +0200

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole:
+      process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false, // Remove console.logs in production except for errors
+  },
 };
 
 export default nextConfig;
