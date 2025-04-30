5d9996ca6a04e8b896781bd5c51401dd5a7e634b
nikeshcohen <nikeshcohen@outlook.com>
2025-04-30 15:54:13 +0200

    decrease header scroll

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -23,7 +23,7 @@ function Header() {
 
   React.useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.scrollY > 50);
+      setIsScrolled(window.scrollY > 5);
     };
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
@@ -39,7 +39,7 @@ function Header() {
           className={cn(
             "mx-auto mt-2 max-w-6xl px-6 transition-all duration-300 lg:px-12",
             isScrolled &&
-              "bg-background/50 max-w-4xl rounded-2xl border backdrop-blur-lg lg:px-5",
+              "bg-background/50 border/70 max-w-4xl rounded-2xl backdrop-blur-lg lg:px-5",
           )}
         >
           <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
