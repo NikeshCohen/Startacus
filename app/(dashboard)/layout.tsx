e08a462d6534a5a3dae98da30136b5cbcb0de213
Nikesh Cohen <nikeshcohen@outlook.com>
2025-10-20 15:33:40 +0200

    fix: dashboard behind auth

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 
 import { Metadata } from "next";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 import DashboardLayoutWrapper from "@/app/(dashboard)/_components/DashboardWrapper";
+
+import { auth } from "@/lib/auth/auth";
 
 export const metadata: Metadata = {
   title: "Dashboard",
 };
 
-function layout({ children }: { children: React.ReactNode }) {
+async function layout({ children }: { children: React.ReactNode }) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in?redirectUrl=/dashboard");
+  }
+
   return <DashboardLayoutWrapper>{children}</DashboardLayoutWrapper>;
 }
 
